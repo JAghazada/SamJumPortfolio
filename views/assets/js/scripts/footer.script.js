9b9298b8ad29f14b66235e5916ac421e089dd4d3
JAghazada <jamil.aghazada03@gmail.com>
2023-07-21 20:37:18 +0400

    samjum dreams came true v3

--- views/assets/js/scripts/footer.script.js
+++ views/assets/js/scripts/footer.script.js
@@ -1,12 +1,8 @@
 const CoolSamJumPhoto = document.querySelector(".coolsamjumphoto");
 
 const DoUWantCallToSamJum = () => {
-    alert()
     const phoneNumber = document.querySelector(".coolsamjumphoto").textContent
     window.location.href = "tel:" + phoneNumber;
 }
 
-CoolSamJumPhoto.addEventListener("click", () => {
-    console.log("x");
-    DoUWantCallToSamJum()
-})
+CoolSamJumPhoto.addEventListener("click", DoUWantCallToSamJum)
